88e270636ac325d4c7c1d3094e6df6f8ecfaad03
Anshul Rath <anshulrath2005@gmail.com>
2026-03-15 03:30:34 +0530

    Updated tools

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,2 +1,2 @@
-from .search import get_search_tool
+from .search import get_search_tools
 from .ecosystem import get_research_tools, firecrawl_scrape, arxiv_full_read_tool, wikipedia_full_read_tool
